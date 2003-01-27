a7b38acc009d209a28b278e47b553f1241be7501
cegger <cegger>
2003-01-27 18:06:45 +0000

    doesn't fix va_list previously declared here warning, but at least the right declaration for the platform is used now

--- include/ggi/internal/debug.h
+++ include/ggi/internal/debug.h
@@ -1,4 +1,4 @@
-/* $Id: debug.h,v 1.2 2002/10/09 21:53:55 cegger Exp $
+/* $Id: debug.h,v 1.3 2003/01/27 18:06:45 cegger Exp $
 ******************************************************************************
 
    LibGGI debugging macros
@@ -28,8 +28,8 @@
 #ifndef _GGI_INTERNAL_DEBUG_H
 #define _GGI_INTERNAL_DEBUG_H
 
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 #include <ggi/types.h>
 #include <ggi/gg.h>
 
