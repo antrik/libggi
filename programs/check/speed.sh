06eaa65137694605bf26ff1c51098d61bb6811aa
soyt <soyt>
2003-03-19 20:12:55 +0000

    the author probably meant trueColor

--- programs/check/speed.sh
+++ programs/check/speed.sh
@@ -8,8 +8,8 @@ GGI_DISPLAY="memory" GGI_DEFMODE="640x480 [P8/8]" ./speed --all
 
 echo
 echo "memory target at 16bpp:"
-GGI_DISPLAY="memory" GGI_DEFMODE="640x480 [T16/16]" ./speed --all
+GGI_DISPLAY="memory" GGI_DEFMODE="640x480 [C16/16]" ./speed --all
 
 echo
 echo "memory target at 24bpp:"
-GGI_DISPLAY="memory" GGI_DEFMODE="640x480 [T24/32]" ./speed --all
+GGI_DISPLAY="memory" GGI_DEFMODE="640x480 [C24/32]" ./speed --all
