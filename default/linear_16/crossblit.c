0cf8e696c384d6442bc24bddc39a35ec642b65c0
cegger <cegger>
2003-04-12 22:32:17 +0000

    gcc 3.3 compiling fix. gcc 3.3 no longer supports multiline strings

--- default/linear_16/crossblit.c
+++ default/linear_16/crossblit.c
@@ -1,4 +1,4 @@
-/* $Id: crossblit.c,v 1.7 2002/10/28 04:57:04 skids Exp $
+/* $Id: crossblit.c,v 1.8 2003/04/12 22:32:17 cegger Exp $
 ******************************************************************************
 
    16-bpp linear direct-access framebuffer renderer for LibGGI:
@@ -1278,68 +1278,68 @@ static inline void cb16to16_mmx(ggi_visual *src, int sx, int sy, int w, int h,
 				while (stopcol > dstp + 11) {
 				  void *dummy;
 				  __asm__ __volatile__(
-				   "movq  (%1), %%mm0\n\t
-                                    pxor  %%mm5, %%mm5\n\t
-                                    movq  8(%1), %%mm1\n\t
-                                    pxor  %%mm6, %%mm6\n\t
-                                    movq  16(%1), %%mm2\n\t
-                                    pxor  %%mm7, %%mm7\n\t
-                                    add   $24, %1\n\t
+				   "movq  (%1), %%mm0\n\t"
+                                   "pxor  %%mm5, %%mm5\n\t"
+                                   "movq  8(%1), %%mm1\n\t"
+                                   "pxor  %%mm6, %%mm6\n\t"
+                                   "movq  16(%1), %%mm2\n\t"
+                                   "pxor  %%mm7, %%mm7\n\t"
+                                   "add   $24, %1\n\t"
 
-                                    .Lleft%=:
-                                    cmp   $0, 8(%2)\n\t
-                                    je    .Lmiddle%=\n\t
+                                   ".Lleft%=:"
+                                   "cmp   $0, 8(%2)\n\t"
+                                   "je    .Lmiddle%=\n\t"
 
-                                    movq  %%mm0, %%mm3\n\t
-                                    movq  %%mm1, %%mm4\n\t
-                                    psllw (%2), %%mm3\n\t
-                                    psllw (%2), %%mm4\n\t
-                                    pand  8(%2), %%mm3\n\t
-                                    pand  8(%2), %%mm4\n\t
-                                    por   %%mm3, %%mm5\n\t
-                                    movq  %%mm2, %%mm3\n\t
-                                    por   %%mm4, %%mm6\n\t
-                                    psllw (%2), %%mm3\n\t
-                                    pand  8(%2), %%mm3\n\t
-                                    cmp   $0, (%2)\n\t
-                                    por   %%mm3, %%mm7\n\t
-                                    je    .Lright%=\n\t
-                                    add   $16, %2\n\t
-                                    jmp   .Lleft%=\n\t
+                                   "movq  %%mm0, %%mm3\n\t"
+                                   "movq  %%mm1, %%mm4\n\t"
+                                   "psllw (%2), %%mm3\n\t"
+                                   "psllw (%2), %%mm4\n\t"
+                                   "pand  8(%2), %%mm3\n\t"
+                                   "pand  8(%2), %%mm4\n\t"
+                                   "por   %%mm3, %%mm5\n\t"
+                                   "movq  %%mm2, %%mm3\n\t"
+                                   "por   %%mm4, %%mm6\n\t"
+                                   "psllw (%2), %%mm3\n\t"
+                                   "pand  8(%2), %%mm3\n\t"
+                                   "cmp   $0, (%2)\n\t"
+                                   "por   %%mm3, %%mm7\n\t"
+                                   "je    .Lright%=\n\t"
+                                   "add   $16, %2\n\t"
+                                   "jmp   .Lleft%=\n\t"
 
-                                    .Lmiddle%=:\n\t
-                                    cmp   $0, (%2)\n\t
-                                    je    .Lright%=\n\t
+                                   ".Lmiddle%=:\n\t"
+                                   "cmp   $0, (%2)\n\t"
+                                   "je    .Lright%=\n\t"
 
-                                    .Ldone%=:\n\t
-                                    movq  %%mm5, (%0)\n\t
-                                    movq  %%mm6, 8(%0)\n\t
-                                    movq  %%mm7, 16(%0)\n\t
-                                    add   $24, %0\n\t
-                                    jmp   .Lout%=\n\t
+                                   ".Ldone%=:\n\t"
+                                   "movq  %%mm5, (%0)\n\t"
+                                   "movq  %%mm6, 8(%0)\n\t"
+                                   "movq  %%mm7, 16(%0)\n\t"
+                                   "add   $24, %0\n\t"
+                                   "jmp   .Lout%=\n\t"
 
-                                    .Lright%=:\n\t
-                                    add   $16, %2\n\t
-                                    cmp   $0, 8(%2)\n\t
-                                    je    .Ldone%=\n\t
+                                   ".Lright%=:\n\t"
+                                   "add   $16, %2\n\t"
+                                   "cmp   $0, 8(%2)\n\t"
+                                   "je    .Ldone%=\n\t"
 
-                                    movq  %%mm0, %%mm3\n\t
-                                    movq  %%mm1, %%mm4\n\t
-                                    psrlw (%2), %%mm3\n\t
-                                    psrlw (%2), %%mm4\n\t
-                                    pand  8(%2), %%mm3\n\t
-                                    pand  8(%2), %%mm4\n\t
-                                    por   %%mm3, %%mm5\n\t
-                                    movq  %%mm2, %%mm3\n\t
-                                    por   %%mm4, %%mm6\n\t
-                                    psrlw (%2), %%mm3\n\t
-                                    pand  8(%2), %%mm3\n\t
-                                    por   %%mm3, %%mm7\n\t
+                                   "movq  %%mm0, %%mm3\n\t"
+                                   "movq  %%mm1, %%mm4\n\t"
+                                   "psrlw (%2), %%mm3\n\t"
+                                   "psrlw (%2), %%mm4\n\t"
+                                   "pand  8(%2), %%mm3\n\t"
+                                   "pand  8(%2), %%mm4\n\t"
+                                   "por   %%mm3, %%mm5\n\t"
+                                   "movq  %%mm2, %%mm3\n\t"
+                                   "por   %%mm4, %%mm6\n\t"
+                                   "psrlw (%2), %%mm3\n\t"
+                                   "pand  8(%2), %%mm3\n\t"
+                                   "por   %%mm3, %%mm7\n\t"
 
-                                    jmp   .Lright%=\n\t
+                                   "jmp   .Lright%=\n\t"
 
-                                    .Lout%=:\n\t
-                                    emms\n\t"
+                                   ".Lout%=:\n\t"
+                                   "emms\n\t"
 				   : "=qom" (dstp), "=qom" (srcp), "=q" (dummy)
 				   : "qom" (dstp), "qom" (srcp), "q" (tab)
 				   : "cc", "memory");
@@ -1438,74 +1438,74 @@ static inline void cb32to16_mmx(ggi_visual *src, int sx, int sy, int w, int h,
 				  void *dummy;
 				  __asm__ __volatile__(
 
-				   "movq  (%1), %%mm0\n\t
-                                    movq  8(%1), %%mm1\n\t
-                                    movq  16(%1), %%mm2\n\t
-                                    movq  24(%1), %%mm3\n\t
-                                    add   $32, %1\n\t
-                                    pxor  %%mm7, %%mm7\n\t
-                                    pxor  %%mm6, %%mm6\n\t
+				   "movq  (%1), %%mm0\n\t"
+                                   "movq  8(%1), %%mm1\n\t"
+                                   "movq  16(%1), %%mm2\n\t"
+                                   "movq  24(%1), %%mm3\n\t"
+                                   "add   $32, %1\n\t"
+                                   "pxor  %%mm7, %%mm7\n\t"
+                                   "pxor  %%mm6, %%mm6\n\t"
 
-                                    .Lleft%=:
-                                    cmp   $0, 8(%2)\n\t
-                                    je    .Lmiddle%=\n\t
+                                   ".Lleft%=:"
+                                   "cmp   $0, 8(%2)\n\t"
+                                   "je    .Lmiddle%=\n\t"
 
-                                    movq  %%mm1, %%mm4\n\t
-                                    movq  %%mm0, %%mm5\n\t
-                                    pslld (%2), %%mm4\n\t
-                                    pslld (%2), %%mm5\n\t
-                                    psrad $16, %%mm4\n\t
-                                    psrad $16, %%mm5\n\t
-                                    packssdw %%mm4, %%mm5\n\t
-                                    movq  %%mm3, %%mm4\n\t
-                                    pand  8(%2), %%mm5\n\t
-                                    por   %%mm5, %%mm6\n\t
-                                    movq  %%mm2, %%mm5\n\t
-                                    pslld (%2), %%mm4\n\t
-                                    pslld (%2), %%mm5\n\t
-                                    psrad $16, %%mm4\n\t
-                                    psrad $16, %%mm5\n\t
-                                    packssdw %%mm4, %%mm5\n\t
-                                    pand  8(%2), %%mm5\n\t
-                                    cmp   $0, (%2)\n\t
-                                    por   %%mm5, %%mm7\n\t
-                                    je    .Lright%=\n\t
-                                    add   $16, %2\n\t
-                                    jmp   .Lleft%=\n\t
+                                   "movq  %%mm1, %%mm4\n\t"
+                                   "movq  %%mm0, %%mm5\n\t"
+                                   "pslld (%2), %%mm4\n\t"
+                                   "pslld (%2), %%mm5\n\t"
+                                   "psrad $16, %%mm4\n\t"
+                                   "psrad $16, %%mm5\n\t"
+                                   "packssdw %%mm4, %%mm5\n\t"
+                                   "movq  %%mm3, %%mm4\n\t"
+                                   "pand  8(%2), %%mm5\n\t"
+                                   "por   %%mm5, %%mm6\n\t"
+                                   "movq  %%mm2, %%mm5\n\t"
+                                   "pslld (%2), %%mm4\n\t"
+                                   "pslld (%2), %%mm5\n\t"
+                                   "psrad $16, %%mm4\n\t"
+                                   "psrad $16, %%mm5\n\t"
+                                   "packssdw %%mm4, %%mm5\n\t"
+                                   "pand  8(%2), %%mm5\n\t"
+                                   "cmp   $0, (%2)\n\t"
+                                   "por   %%mm5, %%mm7\n\t"
+                                   "je    .Lright%=\n\t"
+                                   "add   $16, %2\n\t"
+                                   "jmp   .Lleft%=\n\t"
 
-                                    .Lmiddle%=:\n\t
-                                    cmp   $0, (%2)\n\t
-                                    je    .Lright%=\n\t
+                                   ".Lmiddle%=:\n\t"
+                                   "cmp   $0, (%2)\n\t"
+                                   "je    .Lright%=\n\t"
 
-                                    .Ldone%=:\n\t
-                                    movq  %%mm6, (%0)\n\t
-                                    movq  %%mm7, 8(%0)\n\t
-                                    add   $16, %0\n\t
-                                    jmp   .Lout%=\n\t
+                                   ".Ldone%=:\n\t"
+                                   "movq  %%mm6, (%0)\n\t"
+                                   "movq  %%mm7, 8(%0)\n\t"
+                                   "add   $16, %0\n\t"
+                                   "jmp   .Lout%=\n\t"
 
-                                    .Lright%=:\n\t
-                                    add   $16, %2\n\t
-                                    cmp   $0, 8(%2)\n\t
-                                    je    .Ldone%=\n\t
+                                   ".Lright%=:\n\t"
+                                   "add   $16, %2\n\t"
+                                   "cmp   $0, 8(%2)\n\t"
+                                   "je    .Ldone%=\n\t"
 
-                                    movq  %%mm1, %%mm4\n\t
-                                    movq  %%mm0, %%mm5\n\t
-                                    psrld (%2), %%mm4\n\t
-                                    psrld (%2), %%mm5\n\t
-                                    packssdw %%mm4, %%mm5\n\t
-                                    movq  %%mm3, %%mm4\n\t
-                                    pand  8(%2), %%mm5\n\t
-                                    por   %%mm5, %%mm6\n\t
-                                    movq  %%mm2, %%mm5\n\t
-                                    psrld (%2), %%mm4\n\t
-                                    psrld (%2), %%mm5\n\t
-                                    packssdw %%mm4, %%mm5\n\t
-                                    pand  8(%2), %%mm5\n\t
-                                    por   %%mm5, %%mm7\n\t
-                                    jmp   .Lright%=\n\t
+                                   "movq  %%mm1, %%mm4\n\t"
+                                   "movq  %%mm0, %%mm5\n\t"
+                                   "psrld (%2), %%mm4\n\t"
+                                   "psrld (%2), %%mm5\n\t"
+                                   "packssdw %%mm4, %%mm5\n\t"
+                                   "movq  %%mm3, %%mm4\n\t"
+                                   "pand  8(%2), %%mm5\n\t"
+                                   "por   %%mm5, %%mm6\n\t"
+                                   "movq  %%mm2, %%mm5\n\t"
+                                   "psrld (%2), %%mm4\n\t"
+                                   "psrld (%2), %%mm5\n\t"
+                                   "packssdw %%mm4, %%mm5\n\t"
+                                   "pand  8(%2), %%mm5\n\t"
+                                   "por   %%mm5, %%mm7\n\t"
+                                   "jmp   .Lright%=\n\t"
 
-                                    .Lout%=:\n\t
-                                    emms\n\t"
+                                    ".Lout%=:\n\t"
+                                   "emms\n\t"
 				   : "=qom" (dstp), "=qom" (srcp), "=q" (dummy)
 				   : "qom" (dstp), "qom" (srcp), "q" (tab)
 				   : "cc", "memory");
