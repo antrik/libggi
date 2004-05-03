850a11094b65aa560e7971f981e27a909395a7a3
cegger <cegger>
2004-05-03 22:58:17 +0000

    add testcase, where Peter Ekberg discovered a bug: setting y1 to INT_MIN ends up in an endless-loop. This bug is reproducable now

--- programs/regress/clip2d.c
+++ programs/regress/clip2d.c
@@ -1,4 +1,4 @@
-/* $Id: clip2d.c,v 1.1 2004/05/03 22:39:41 cegger Exp $
+/* $Id: clip2d.c,v 1.2 2004/05/03 22:58:17 cegger Exp $
 ******************************************************************************
 
    This is a regression-test and for LibGGI clipping operations.
@@ -104,6 +104,81 @@ static void testcase1(void)
 }
 
 
+static void testcase2(void)
+{
+	int x0, x1, y0, y1, clip_first, clip_last;
+	int ret;
+
+	int x0_expect = 50;
+	int y0_expect = 50;
+	int x1_expect = 52;
+	int y1_expect = 52;
+	int clip_first_expect = 0;
+	int clip_last_expect = 0;
+	int ret_expect = 1;
+
+	x0 = 50;
+	y0 = 50;
+	x1 = 52;
+	y1 = INT_MIN;
+
+
+	printteststart(__PRETTY_FUNCTION__);
+
+	ret = _ggi_clip2d(vis, &x0, &y0, &x1, &y1,
+			&clip_first, &clip_last);
+
+	if (ret != ret_expect) {
+		printfailure("expected return value: \"%i\"\n"
+			"actual return value: \"%i\"\n",
+			ret_expect, ret);
+		return;
+	}
+
+
+	if (x0 != x0_expect) {
+		printfailure("expected x0 value: \"%i\"\n"
+			"actual x0 value: \"%i\"\n",
+			x0_expect, x0);
+		return;
+	}
+	if (x1 != x1_expect) {
+		printfailure("expected x1 value: \"%i\"\n"
+			"actual x1 value: \"%i\"\n",
+			x1_expect, x1);
+		return;
+	}
+	if (y0 != y0_expect) {
+		printfailure("expected y0 value: \"%i\"\n"
+			"actual y0 value: \"%i\"\n",
+			y0_expect, y0);
+		return;
+	}
+	if (y1 != y1_expect) {
+		printfailure("expected y1 value: \"%i\"\n"
+			"actual y1 value: \"%i\"\n",
+			y1_expect, y1);
+		return;
+	}
+	if (clip_first != clip_first_expect) {
+		printfailure("expected clip_first value: \"%i\"\n"
+			"actual clip_first value: \"%i\"\n",
+			clip_first_expect, clip_first);
+		return;
+	}
+	if (clip_last != clip_last_expect) {
+		printfailure("expected clip_last value: \"%i\"\n"
+			"actual clip_last value: \"%i\"\n",
+			clip_last_expect, clip_last);
+		return;
+	}
+
+	printsuccess();
+	return;
+}
+
+
+
 int main(void)
 {
 	int rc;
@@ -120,6 +195,7 @@ int main(void)
 
 	/* run tests */
 	testcase1();
+	testcase2();
 
 	rc = ggiClose(vis);
 
