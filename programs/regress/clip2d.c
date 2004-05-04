b45ab2701d4c9d737abf79901d7c66693e900b13
cegger <cegger>
2004-05-04 10:39:34 +0000

    unify result checking. Tweak testcase2 to test the theoretical worst case that API allows.

--- programs/regress/clip2d.c
+++ programs/regress/clip2d.c
@@ -1,4 +1,4 @@
-/* $Id: clip2d.c,v 1.3 2004/05/04 10:23:24 cegger Exp $
+/* $Id: clip2d.c,v 1.4 2004/05/04 10:39:34 cegger Exp $
 ******************************************************************************
 
    This is a regression-test and for LibGGI clipping operations.
@@ -28,12 +28,67 @@
 #include "common.inc.c"
 
 
+#define MODE_SIZE_X	640
+#define MODE_SIZE_Y	480
+
+
 static ggi_visual_t vis;
+
+
+static int checkresult(int x0, int y0, int x1, int y1, int clip_first, int clip_last,
+			int x0_expect, int y0_expect, int x1_expect, int y1_expect,
+			int clip_first_expect, int clip_last_expect)
+{
+	if (x0 != x0_expect) {
+		printfailure("expected x0 value: \"%i\"\n"
+			"actual x0 value: \"%i\"\n",
+			x0_expect, x0);
+		return -1;
+	}
+	if (x1 != x1_expect) {
+		printfailure("expected x1 value: \"%i\"\n"
+			"actual x1 value: \"%i\"\n",
+			x1_expect, x1);
+		return -1;
+	}
+	if (y0 != y0_expect) {
+		printfailure("expected y0 value: \"%i\"\n"
+			"actual y0 value: \"%i\"\n",
+			y0_expect, y0);
+		return -1;
+	}
+	if (y1 != y1_expect) {
+		printfailure("expected y1 value: \"%i\"\n"
+			"actual y1 value: \"%i\"\n",
+			y1_expect, y1);
+		return -1;
+	}
+	if (clip_first != clip_first_expect) {
+		printfailure("expected clip_first value: \"%i\"\n"
+			"actual clip_first value: \"%i\"\n",
+			clip_first_expect, clip_first);
+		return -1;
+	}
+	if (clip_last != clip_last_expect) {
+		printfailure("expected clip_last value: \"%i\"\n"
+			"actual clip_last value: \"%i\"\n",
+			clip_last_expect, clip_last);
+		return -1;
+	}
+
+	return 0;
+}
+
 
 
 static void testcase1(void)
 {
-	int x0, x1, y0, y1, clip_first, clip_last;
+	int x0 = 50;
+	int y0 = 50;
+	int x1 = 52;
+	int y1 = 52;
+	int clip_first = 0;
+	int clip_last = 0;
 	int ret;
 
 	int x0_expect = 50;
@@ -43,11 +98,6 @@ static void testcase1(void)
 	int clip_first_expect = 0;
 	int clip_last_expect = 0;
 	int ret_expect = 1;
-
-	x0 = 50;
-	y0 = 50;
-	x1 = 52;
-	y1 = 52;
 
 
 	printteststart(__PRETTY_FUNCTION__);
@@ -62,43 +112,10 @@ static void testcase1(void)
 		return;
 	}
 
-
-	if (x0 != x0_expect) {
-		printfailure("expected x0 value: \"%i\"\n"
-			"actual x0 value: \"%i\"\n",
-			x0_expect, x0);
-		return;
-	}
-	if (x1 != x1_expect) {
-		printfailure("expected x1 value: \"%i\"\n"
-			"actual x1 value: \"%i\"\n",
-			x1_expect, x1);
-		return;
-	}
-	if (y0 != y0_expect) {
-		printfailure("expected y0 value: \"%i\"\n"
-			"actual y0 value: \"%i\"\n",
-			y0_expect, y0);
-		return;
-	}
-	if (y1 != y1_expect) {
-		printfailure("expected y1 value: \"%i\"\n"
-			"actual y1 value: \"%i\"\n",
-			y1_expect, y1);
-		return;
-	}
-	if (clip_first != clip_first_expect) {
-		printfailure("expected clip_first value: \"%i\"\n"
-			"actual clip_first value: \"%i\"\n",
-			clip_first_expect, clip_first);
-		return;
-	}
-	if (clip_last != clip_last_expect) {
-		printfailure("expected clip_last value: \"%i\"\n"
-			"actual clip_last value: \"%i\"\n",
-			clip_last_expect, clip_last);
-		return;
-	}
+	ret = checkresult(x0, y0, x1, y1, clip_first, clip_last,
+		x0_expect, y0_expect, x1_expect, y1_expect,
+		clip_first_expect, clip_last_expect);
+	if (ret != 0) return;
 
 	printsuccess();
 	return;
@@ -107,21 +124,21 @@ static void testcase1(void)
 
 static void testcase2(void)
 {
-	int x0, x1, y0, y1, clip_first, clip_last;
+	int x0 = INT_MIN;
+	int y0 = INT_MIN;
+	int x1 = INT_MAX;
+	int y1 = INT_MAX;
+	int clip_first = 0;
+	int clip_last = 0;
 	int ret;
 
-	int x0_expect = 50;
-	int y0_expect = 50;
-	int x1_expect = 52;
-	int y1_expect = 52;
+	int x0_expect = 0;
+	int y0_expect = 0;
+	int x1_expect = MODE_SIZE_X - 1;
+	int y1_expect = MODE_SIZE_Y - 1;
 	int clip_first_expect = 0;
 	int clip_last_expect = 0;
 	int ret_expect = 1;
-
-	x0 = 50;
-	y0 = 50;
-	x1 = 52;
-	y1 = INT_MIN;
 
 
 	printteststart(__PRETTY_FUNCTION__);
@@ -136,43 +153,10 @@ static void testcase2(void)
 		return;
 	}
 
-
-	if (x0 != x0_expect) {
-		printfailure("expected x0 value: \"%i\"\n"
-			"actual x0 value: \"%i\"\n",
-			x0_expect, x0);
-		return;
-	}
-	if (x1 != x1_expect) {
-		printfailure("expected x1 value: \"%i\"\n"
-			"actual x1 value: \"%i\"\n",
-			x1_expect, x1);
-		return;
-	}
-	if (y0 != y0_expect) {
-		printfailure("expected y0 value: \"%i\"\n"
-			"actual y0 value: \"%i\"\n",
-			y0_expect, y0);
-		return;
-	}
-	if (y1 != y1_expect) {
-		printfailure("expected y1 value: \"%i\"\n"
-			"actual y1 value: \"%i\"\n",
-			y1_expect, y1);
-		return;
-	}
-	if (clip_first != clip_first_expect) {
-		printfailure("expected clip_first value: \"%i\"\n"
-			"actual clip_first value: \"%i\"\n",
-			clip_first_expect, clip_first);
-		return;
-	}
-	if (clip_last != clip_last_expect) {
-		printfailure("expected clip_last value: \"%i\"\n"
-			"actual clip_last value: \"%i\"\n",
-			clip_last_expect, clip_last);
-		return;
-	}
+	ret = checkresult(x0, y0, x1, y1, clip_first, clip_last,
+		x0_expect, y0_expect, x1_expect, y1_expect,
+		clip_first_expect, clip_last_expect);
+	if (ret != 0) return;
 
 	printsuccess();
 	return;
@@ -190,7 +174,7 @@ int main(void)
 	vis = ggiOpen("display-memory", NULL);
 	if (!vis) return 1;
 
-	rc = ggiCheckSimpleMode(vis, 640, 480, 1, GT_AUTO, &tm);
+	rc = ggiCheckSimpleMode(vis, MODE_SIZE_X, MODE_SIZE_Y, 1, GT_AUTO, &tm);
 	rc = ggiSetMode(vis, &tm);
 	if (rc < 0) ggiPanic("Mode initialization failed.");
 
