c5f552be6e6151bd85a7ac996c302cf9481ce108
cegger <cegger>
2004-09-10 16:38:47 +0000

    add testcase5 and 6 that reproduces the bug Peter Eckberg recently found in display-directx

--- programs/regress/init.c
+++ programs/regress/init.c
@@ -1,4 +1,4 @@
-/* $Id: init.c,v 1.3 2004/08/27 15:17:35 cegger Exp $
+/* $Id: init.c,v 1.4 2004/09/10 16:38:47 cegger Exp $
 ******************************************************************************
 
    This is a regression-test for init/exit handling.
@@ -101,9 +101,16 @@ static void testcase3(const char *desc)
 
 	err = ggiClose(vis);
 	if (err != GGI_OK) {
-		printfailure("expected return value: %i\n"
+		printfailure("ggiClose: expected return value: %i\n"
 			"actual return value: %i\n",
 			GGI_OK, err);
+		return;
+	}
+
+	err = ggiExit();
+	if (err != 0) {
+		printfailure("ggiExit: expected return value: 0\n"
+			"actual return value: %i\n", err);
 		return;
 	}
 
@@ -120,7 +127,7 @@ static void testcase4(const char *desc)
 
 	err = ggiClose(NULL);
 	if (err != GGI_EARGINVAL) {
-		printfailure("expected return value: %i\n"
+		printfailure("ggiClose: expected return value: %i\n"
 			"actual return value: %i\n",
 			GGI_EARGINVAL, err);
 		return;
@@ -129,6 +136,138 @@ static void testcase4(const char *desc)
 	printsuccess();
 	return;
 }
+
+static void testcase5(const char *desc)
+{
+	int err;
+	ggi_visual_t vis;
+
+	printteststart(__FILE__, __PRETTY_FUNCTION__, EXPECTED2PASS, desc);
+	if (dontrun) return;
+
+	err = ggiInit();
+	if (err != GGI_OK) {
+		printfailure("ggiInit: expected return value: 0\n"
+			"actual return value: %i\n", err);
+		return;
+	}
+
+
+	vis = ggiOpen(NULL);
+	if (vis == NULL) {
+		printfailure("ggiOpen: Attempt 1: Couldn\'t open default visual.");
+		return;
+	}
+
+	err = ggiClose(vis);
+	if (err != GGI_OK) {
+		printfailure("ggiClose: Attempt 1: expected return value: %i\n"
+			"actual return value: %i\n",
+			GGI_OK, err);
+		return;
+	}
+
+
+	vis = ggiOpen(NULL);
+	if (vis == NULL) {
+		printfailure("ggiOpen: Attempt 2: Couldn\'t open default visual again.");
+		return;
+	}
+
+	err = ggiClose(vis);
+	if (err != GGI_OK) {
+		printfailure("ggiClose: Attempt 2: expected return value: %i\n"
+			"actual return value: %i\n",
+			GGI_OK, err);
+		return;
+	}
+
+
+	err = ggiExit();
+	if (err != 0) {
+		printfailure("ggiExit: expected return value: 0\n"
+			"actual return value: %i\n", err);
+		return;
+	}
+
+	printsuccess();
+	return;
+}
+
+
+static void testcase6(const char *desc)
+{
+	int err;
+	ggi_visual_t vis, vis2;
+
+	printteststart(__FILE__, __PRETTY_FUNCTION__, EXPECTED2PASS, desc);
+	if (dontrun) return;
+
+	err = ggiInit();
+	if (err != GGI_OK) {
+		printfailure("ggiInit: expected return value: 0\n"
+			"actual return value: %i\n", err);
+		return;
+	}
+
+	vis2 = ggiOpen(NULL);
+	if (vis2 == NULL) {
+		printfailure("ggiOpen: visual 2: Couldn\'t open default visual again.");
+		return;
+	}
+
+
+	vis = ggiOpen(NULL);
+	if (vis == NULL) {
+		printfailure("ggiOpen: visual 1: Attempt 1: Couldn\'t open default visual.");
+		return;
+	}
+
+	err = ggiClose(vis);
+	if (err != GGI_OK) {
+		printfailure("ggiClose: visual 1: Attempt 1: expected return value: %i\n"
+			"actual return value: %i\n",
+			GGI_OK, err);
+		return;
+	}
+
+
+	vis = ggiOpen(NULL);
+	if (vis == NULL) {
+		printfailure("ggiOpen: visual 1: Attempt 2: Couldn\'t open default visual.");
+		return;
+	}
+
+	err = ggiClose(vis);
+	if (err != GGI_OK) {
+		printfailure("ggiClose: visual 1: Attempt 2: expected return value: %i\n"
+			"actual return value: %i\n",
+			GGI_OK, err);
+		return;
+	}
+
+
+	err = ggiClose(vis2);
+	if (err != GGI_OK) {
+		printfailure("ggiClose: visual 2: expected return value: %i\n"
+			"actual return value: %i\n",
+			GGI_OK, err);
+		return;
+	}
+
+
+	err = ggiExit();
+	if (err != 0) {
+		printfailure("ggiExit: expected return value: 0\n"
+			"actual return value: %i\n", err);
+		return;
+	}
+
+
+	printsuccess();
+	return;
+}
+
 
 
 
@@ -141,6 +280,8 @@ int main(int argc, char * const argv[])
 	testcase2("Check that ggiExit() behaves as documented.");
 	testcase3("Check ggiOpen()/ggiClose() to open/close a default visual.");
 	testcase4("Check ggiClose() works correct with an invalid argument.");
+	testcase5("ggiOpen()/ggiClose() a default visual twice times using the same visual variable.");
+	testcase6("As testcase5, but kind of nested in a second visual");
 
 	printsummary();
 
