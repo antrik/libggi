deef2ab01abf329960cf6f718166ac95f1be691e
cegger <cegger>
2004-09-18 09:46:41 +0000

    add rcs identifer. Change license from LGPL to BSD pico ll.h

--- include/ggi/ll.h
+++ include/ggi/ll.h
@@ -1,27 +1,31 @@
-/*
+/* $Id: ll.h,v 1.4 2004/09/18 09:46:41 cegger Exp $
 ******************************************************************************
 
    Private structures common to lowlevel libraries.
 
-   Copyright (c) Tue Mar 18 2002 by:
-        Brian S. Julin          bri@tull.umassp.edu
-        Christoph Egger         Christoph_Egger@t-online.de
+   Copyright (C) 2002		Brian S. Julin
+   Copyright (C) 2002		Christoph Egger
 
+   Permission is hereby granted, free of charge, to any person obtaining a
+   copy of this software and associated documentation files (the "Software"),
+   to deal in the Software without restriction, including without limitation
+   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+   and/or sell copies of the Software, and to permit persons to whom the
+   Software is furnished to do so, subject to the following conditions:
 
-This software may be redistributed under the terms of the GNU Library
-(a.k.a. "Lesser") Public License -- see the file COPYING in the parent
-directory of the source code.
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
-THE AUTHOR(S) BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+   THE AUTHOR(S) BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+   IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 ******************************************************************************
 */
+
 
 #ifndef _GGI_LL_H
 #define _GGI_LL_H
