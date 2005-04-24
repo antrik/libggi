e3942800ffc1a8fbefb6f3eb8edd03702254325b
cegger <cegger>
2005-04-24 09:25:11 +0000

    sync up with libtool cvs branch-2-0 to version 1.1167.2.219

--- ltmain.sh
+++ ltmain.sh
@@ -1,6 +1,6 @@
 # Generated from ltmain.m4sh; do not edit by hand
 
-# ltmain.sh (GNU libtool 1.1667.2.211 2005/04/15 14:39:25) 1.9g
+# ltmain.sh (GNU libtool 1.1667.2.219 2005/04/23 15:45:06) 1.9g
 # Written by Gordon Matzigkeit <gord@gnu.ai.mit.edu>, 1996
 
 # Copyright (C) 1996, 1997, 1998, 1999, 2000, 2001, 2003, 2004, 2005 Free Software Foundation, Inc.
@@ -19,7 +19,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #
 # As a special exception to the GNU General Public License, if you
 # distribute this file as part of a program that contains a
@@ -63,7 +63,7 @@
 #       compiler:		$LTCC
 #       compiler flags:		$LTCFLAGS
 #       linker:		$LD (gnu? $with_gnu_ld)
-#       $progname:		(GNU libtool 1.1667.2.211 2005/04/15 14:39:25) 1.9g
+#       $progname:		(GNU libtool 1.1667.2.219 2005/04/23 15:45:06) 1.9g
 #       automake:		$automake_version
 #       autoconf:		$autoconf_version
 #
@@ -72,8 +72,8 @@
 PROGRAM=ltmain.sh
 PACKAGE=libtool
 VERSION=1.9g
-TIMESTAMP=" 1.1667.2.211 2005/04/15 14:39:25"
-package_revision=1.1667.2.211
+TIMESTAMP=" 1.1667.2.219 2005/04/23 15:45:06"
+package_revision=1.1667.2.219
 
 ## --------------------- ##
 ## M4sh Initialization.  ##
--- m4/ltversion.m4
+++ m4/ltversion.m4
@@ -9,15 +9,15 @@
 
 # Generated from ltversion.in; do not edit by hand
 
-# serial 1667.2.211
+# serial 1667.2.219
 # This file is part of GNU Libtool
 
 m4_define([LT_PACKAGE_VERSION], [1.9g])
-m4_define([LT_PACKAGE_REVISION], [1.1667.2.211])
+m4_define([LT_PACKAGE_REVISION], [1.1667.2.219])
 
 AC_DEFUN([LTVERSION_VERSION],
 [macro_version='1.9g'
-macro_revision='1.1667.2.211'
+macro_revision='1.1667.2.219'
 _LT_DECL(, macro_version, 0, [Which release of libtool.m4 was used?])
 _LT_DECL(, macro_revision, 0)
 ])
