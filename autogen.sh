1a03381562d914feb09627ac5717f3a08cd9f990
pekberg <pekberg>
2005-09-05 13:25:29 +0000

    Add --force option that disables version checks.

--- autogen.sh
+++ autogen.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
 
-AC_VERSION="2.59"
-AM_VERSION="1.9.6"
+version_check=yes
 
-printf "Checking for required autoconf ${AC_VERSION}..."
-./checkversion.sh autoconf $AC_VERSION
-test $? -eq 0 || exit $? && printf "ok\n"
-printf "Checking for required automake ${AM_VERSION}..."
-./checkversion.sh automake $AM_VERSION
-test $? -eq 0 || exit $? && printf "ok\n"
+for opt; do
+  case "$opt" in
+  --force)
+    version_check=no
+    ;;
+  esac
+done
 
+if test $version_check = yes; then
+  AC_VERSION="2.59"
+  AM_VERSION="1.9.6"
+
+  printf "Checking for required autoconf ${AC_VERSION}..."
+  ./checkversion.sh autoconf $AC_VERSION
+  test $? -eq 0 || exit $? && printf "ok\n"
+  printf "Checking for required automake ${AM_VERSION}..."
+  ./checkversion.sh automake $AM_VERSION
+  test $? -eq 0 || exit $? && printf "ok\n"
+fi
 
 touch ChangeLog
 
