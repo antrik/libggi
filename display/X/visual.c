5a822413ede15d9929a61cac6faddc9347b17b9e
cegger <cegger>
2005-10-08 12:49:55 +0000

    fix memory leak found with valgrind

--- display/X/visual.c
+++ display/X/visual.c
@@ -1,4 +1,4 @@
-/* $Id: visual.c,v 1.50 2005/09/19 18:46:40 cegger Exp $
+/* $Id: visual.c,v 1.51 2005/10/08 12:49:55 cegger Exp $
 ******************************************************************************
 
    LibGGI Display-X target: initialization
@@ -253,7 +253,10 @@ skip3:
 
 	DPRINT_MISC("GGIclose: free mansync\n");
 	if (priv->opmansync)	    free(priv->opmansync);
+
  skip2:
+	DPRINT_MISC("GGIclose: free gc\n");
+	if (priv->gc)		    XFreeGC(priv->disp, priv->gc);
 	DPRINT_MISC("GGIclose: destroy flushlock\n");
 	if (priv->flushlock)	    ggLockDestroy(priv->flushlock);
 	DPRINT_MISC("GGIclose: destroy xliblock\n");
