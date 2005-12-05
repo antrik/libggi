4aa53cdf18496aacaf23f84c6e511c0c980d96f8
cegger <cegger>
2005-12-05 22:19:13 +0000

    sync with libtool cvs head to version 1.2216

--- ltmain.sh
+++ ltmain.sh
@@ -1,6 +1,6 @@
 # Generated from ltmain.m4sh; do not edit by hand
 
-# ltmain.sh (GNU libtool 1.2205 2005/11/27 15:18:09) 2.1a
+# ltmain.sh (GNU libtool 1.2216 2005/12/04 17:57:45) 2.1a
 # Written by Gordon Matzigkeit <gord@gnu.ai.mit.edu>, 1996
 
 # Copyright (C) 1996, 1997, 1998, 1999, 2000, 2001, 2003, 2004, 2005 Free Software Foundation, Inc.
@@ -63,7 +63,7 @@
 #       compiler:		$LTCC
 #       compiler flags:		$LTCFLAGS
 #       linker:		$LD (gnu? $with_gnu_ld)
-#       $progname:		(GNU libtool 1.2205 2005/11/27 15:18:09) 2.1a
+#       $progname:		(GNU libtool 1.2216 2005/12/04 17:57:45) 2.1a
 #       automake:		$automake_version
 #       autoconf:		$autoconf_version
 #
@@ -72,8 +72,8 @@
 PROGRAM=ltmain.sh
 PACKAGE=libtool
 VERSION=2.1a
-TIMESTAMP=" 1.2205 2005/11/27 15:18:09"
-package_revision=1.2205
+TIMESTAMP=" 1.2216 2005/12/04 17:57:45"
+package_revision=1.2216
 
 ## --------------------- ##
 ## M4sh Initialization.  ##
@@ -460,7 +460,13 @@ func_show_eval ()
       eval "func_echo $func_quote_for_expand_result"
     }
 
-    ${opt_dry_run-false} || eval "$my_cmd" || eval "$my_fail_exp"
+    if ${opt_dry_run-false}; then :; else
+      eval "$my_cmd"
+      my_status=$?
+      if test "$my_status" -eq 0; then :; else
+	eval "(exit $my_status); $my_fail_exp"
+      fi
+    fi
 }
 
 
@@ -1408,9 +1414,16 @@ static const void *lt_preloaded_setup() {
 	  esac
 	  ;;
 	esac
+	symtab_cflags=
+	for arg in $LTCFLAGS; do
+	  case $arg in
+	  -pie | -fpie | -fPIE) ;;
+	  *) symtab_cflags="$symtab_cflags $arg" ;;
+	  esac
+	done
 
 	# Now compile the dynamic symbol file.
-	func_show_eval '(cd $output_objdir && $LTCC $LTCFLAGS -c$no_builtin_flag$pic_flag_for_symtable "$my_dlsyms")' 'exit $?'
+	func_show_eval '(cd $output_objdir && $LTCC$symtab_cflags -c$no_builtin_flag$pic_flag_for_symtable "$my_dlsyms")' 'exit $?'
 
 	# Clean up the generated files.
 	func_show_eval '$RM "$output_objdir/$my_dlsyms" "$nlist" "${nlist}S" "${nlist}T"'
@@ -1454,7 +1467,7 @@ func_extract_an_archive ()
     $opt_debug
     f_ex_an_ar_dir="$1"; shift
     f_ex_an_ar_oldlib="$1"
-    func_show_eval "(cd \$f_ex_an_ar_dir && $AR x \$f_ex_an_ar_oldlib)" || exit $?
+    func_show_eval "(cd \$f_ex_an_ar_dir && $AR x \$f_ex_an_ar_oldlib)" 'exit $?'
     if ($AR t "$f_ex_an_ar_oldlib" | sort | sort -uc >/dev/null 2>&1); then
      :
     else
@@ -1548,6 +1561,7 @@ func_mode_compile ()
     arg_mode=normal
     libobj=
     later=
+    pie_flag=
 
     for arg
     do
@@ -1571,6 +1585,11 @@ func_mode_compile ()
 	  test -n "$libobj" && \
 	    func_fatal_error "you cannot specify \`-o' more than once"
 	  arg_mode=target
+	  continue
+	  ;;
+
+	-pie | -fpie | -fPIE)
+          pie_flag="$pie_flag $arg"
 	  continue
 	  ;;
 
@@ -1863,7 +1882,7 @@ EOF
     if test "$build_old_libs" = yes; then
       if test "$pic_mode" != yes; then
 	# Don't build PIC code
-	command="$base_compile $qsrcfile"
+	command="$base_compile $qsrcfile$pie_flag"
       else
 	command="$base_compile $qsrcfile $pic_flag"
       fi
@@ -6023,15 +6042,15 @@ EOF
 	link_command="$compile_command$compile_rpath"
 
 	# We have no uninstalled library dependencies, so finalize right now.
-	func_show_eval "$link_command"
-	status=$?
+	exit_status=0
+	func_show_eval "$link_command" 'exit_status=$?'
 
 	# Delete the generated files.
 	if test -f "$output_objdir/${outputname}S.${objext}"; then
 	  func_show_eval '$RM "$output_objdir/${outputname}S.${objext}"'
 	fi
 
-	exit $status
+	exit $exit_status
       fi
 
       if test -n "$compile_shlibpath$finalize_shlibpath"; then
