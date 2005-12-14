2af33d9751c9724baf5e883daecfac52a8698cad
cegger <cegger>
2005-12-14 21:48:46 +0000

    sync with libtool cvs head to version 1.2220

--- ltmain.sh
+++ ltmain.sh
@@ -1,6 +1,6 @@
 # Generated from ltmain.m4sh; do not edit by hand
 
-# ltmain.sh (GNU libtool 1.2216 2005/12/04 17:57:45) 2.1a
+# ltmain.sh (GNU libtool 1.2220 2005/12/08 17:27:08) 2.1a
 # Written by Gordon Matzigkeit <gord@gnu.ai.mit.edu>, 1996
 
 # Copyright (C) 1996, 1997, 1998, 1999, 2000, 2001, 2003, 2004, 2005 Free Software Foundation, Inc.
@@ -63,7 +63,7 @@
 #       compiler:		$LTCC
 #       compiler flags:		$LTCFLAGS
 #       linker:		$LD (gnu? $with_gnu_ld)
-#       $progname:		(GNU libtool 1.2216 2005/12/04 17:57:45) 2.1a
+#       $progname:		(GNU libtool 1.2220 2005/12/08 17:27:08) 2.1a
 #       automake:		$automake_version
 #       autoconf:		$autoconf_version
 #
@@ -72,8 +72,8 @@
 PROGRAM=ltmain.sh
 PACKAGE=libtool
 VERSION=2.1a
-TIMESTAMP=" 1.2216 2005/12/04 17:57:45"
-package_revision=1.2216
+TIMESTAMP=" 1.2220 2005/12/08 17:27:08"
+package_revision=1.2220
 
 ## --------------------- ##
 ## M4sh Initialization.  ##
@@ -2653,7 +2653,7 @@ func_mode_link ()
     vinfo=
     vinfo_number=no
     weak_libs=
-
+    single_module="${wl}-single_module"
     func_infer_tag $base_compile
 
     # We need to know -static, to get the right output filenames.
@@ -3118,6 +3118,11 @@ func_mode_link ()
 	    *" $arg "*) ;;
 	    * ) new_inherited_linker_flags="$new_inherited_linker_flags $arg" ;;
 	esac
+	continue
+	;;
+
+      -multi_module)
+	single_module="${wl}-multi_module"
 	continue
 	;;
 
