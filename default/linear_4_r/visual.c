8cdb89afe209459d71fd9eba46a5d9e8f27f3a8a
cegger <cegger>
2006-01-30 20:32:47 +0000

    kill conversion of function to object pointer

--- default/linear_4_r/visual.c
+++ default/linear_4_r/visual.c
@@ -1,4 +1,4 @@
-/* $Id: visual.c,v 1.6 2005/07/30 11:40:02 cegger Exp $
+/* $Id: visual.c,v 1.7 2006/01/30 20:32:47 cegger Exp $
 ******************************************************************************
 
    Graphics library for GGI.
@@ -80,9 +80,12 @@ int GGIdl_linear_4_r(int func, void **funcptr);
 
 int GGIdl_linear_4_r(int func, void **funcptr)
 {
+	ggifunc_open **openptr;
+
 	switch (func) {
 	case GGIFUNC_open:
-		*funcptr = (void *)GGIopen;
+		openptr = (ggifunc_open **)funcptr;
+		*openptr = GGIopen;
 		return 0;
 	case GGIFUNC_exit:
 	case GGIFUNC_close:
