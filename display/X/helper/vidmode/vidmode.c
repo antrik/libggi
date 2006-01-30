2397e9299dd505268228f94bcf7034ecfc2ed3a9
pekberg <pekberg>
2006-01-30 13:44:50 +0000

    Kill conversion of function pointer to object pointer.

--- display/X/helper/vidmode/vidmode.c
+++ display/X/helper/vidmode/vidmode.c
@@ -1,4 +1,4 @@
-/* $Id: vidmode.c,v 1.27 2005/08/17 13:08:13 mooz Exp $
+/* $Id: vidmode.c,v 1.28 2006/01/30 13:44:50 pekberg Exp $
 ******************************************************************************
 
    XFree86-VidMode extension support for display-x
@@ -512,15 +512,20 @@ EXPORTFUNC int GGIdl_helper_x_vidmode(int func, void **funcptr);
 
 int GGIdl_helper_x_vidmode(int func, void **funcptr)
 {
+	ggifunc_open **openptr;
+	ggifunc_close **closeptr;
+
 	switch (func) {
 	case GGIFUNC_open:
-		*funcptr = (void *) GGIopen;
+		openptr = (ggifunc_open **)funcptr;
+		*openptr = GGIopen;
 		return GGI_OK;
 	case GGIFUNC_exit:
 		*funcptr = NULL;
 		return GGI_OK;
 	case GGIFUNC_close:
-		*funcptr = (void *) GGIclose;
+		closeptr = (ggifunc_close **)funcptr;
+		*closeptr = GGIclose;
 		return GGI_OK;
 	default:
 		*funcptr = NULL;
