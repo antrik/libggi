22b0d7011a0d321f24880640474f0b70c47f3631
pekberg <pekberg>
2006-12-31 04:17:41 +0000

    Adjust to new world order.

--- programs/regress/input.c
+++ programs/regress/input.c
@@ -1,4 +1,4 @@
-/* $Id: input.c,v 1.8 2006/12/30 16:17:06 cegger Exp $
+/* $Id: input.c,v 1.9 2006/12/31 04:17:41 pekberg Exp $
 ******************************************************************************
 
    This is a regression-test for visual <-> input association.
@@ -31,12 +31,10 @@ typedef struct ggi_originbounds {
 
 ggi_visual_t vis;
 ggi_mode mode;
-gii_input inp;
 
 static int ggi_findorigin(ggi_visual_t visual,ggi_originbounds_t *origins)
 {
 	struct gii_source_iter src_iter;
-	struct gii_device_iter dev_iter;
 	uint32_t origin;
 
 	origins->first = 0xffffffff;
@@ -45,15 +43,9 @@ static int ggi_findorigin(ggi_visual_t visual,ggi_originbounds_t *origins)
 	src_iter.stem = visual;
 	giiIterSources(&src_iter);
 	GG_ITER_FOREACH(&src_iter) {
-		dev_iter.stem = visual;
-		dev_iter.src = src_iter.src;
-		giiIterDevices(&dev_iter);
-		GG_ITER_FOREACH(&dev_iter) {
-			origin = dev_iter.origin;
-			if (origins->first > origin) origins->first = origin;
-			if (origins->last  < origin) origins->last  = origin;
-		}
-		GG_ITER_DONE(&dev_iter);
+		origin = src_iter.origin;
+		if (origins->first > origin) origins->first = origin;
+		if (origins->last  < origin) origins->last  = origin;
 	}
 	GG_ITER_DONE(&src_iter);
 	return (origins->first > origins->last);
@@ -66,12 +58,10 @@ static int precase(void)
 
 	if (dontrun) return 0;
 
-	ggiInit();
-
-	vis = ggNewStem(libggi, NULL);
+	vis = ggNewStem(libgii, libggi, NULL);
 	if (vis == NULL) {
 		printfailure("ggiOpen: Couldn\'t create and attach LibGGI to stem.\n");
-		return err;
+		return GGI_EUNKNOWN;
 	}
 
 	err = ggiOpen(vis, NULL);
@@ -100,18 +90,17 @@ static int postcase(void)
 	}
 	ggDelStem(vis);
 
-	err = ggiExit();
-
 	return 0;
 }
 
 
 static int ggi_opentestvis(ggi_visual_t *visual, ggi_originbounds_t *ori, 
-			   gii_input *input, const char *visname)
+			   struct gg_stem *input, const char *visname)
 {
+	uint32_t origins;
 	int err;
 
-	*visual = ggNewStem(libggi, NULL);
+	*visual = ggNewStem(libgii, libggi, NULL);
 	printassert(*visual != NULL, "ggNewStem failed\n");
 
 	err = ggiOpen(*visual, NULL);
@@ -130,55 +119,37 @@ static int ggi_opentestvis(ggi_visual_t *visual, ggi_originbounds_t *ori,
 		return -1;
 	}
 
-	*input = giiJoinInputs(*input, ggiDetachInput(*visual));
-	if (*input == NULL) {
-		printfailure("joined inps: expected return value: \"non-NULL\"\n"
-			"actual return value: \"NULL\"\n");
-		return -1;
+	for (origins = ori->first; origins <= ori->last; origins++) {
+		err = giiTransfer(*visual, input, origins);
+		if (err != GGI_OK) {
+			printfailure("joined inps: expected return value: \"GGI_OK\"\n"
+				"actual return value: \"%i\"\n", err);
+			return -1;
+		}
 	}
 	return 0;
 }
 
 static int ggi_closetestvis(ggi_visual_t *visual,ggi_originbounds_t *ori, 
-			    gii_input *input, const char *visname,int islast)
+			    struct gg_stem *input, const char *visname,int islast)
 {
 	uint32_t origins;
-	gii_input newhand;
-	int haveone = 0;
-	int errcode;
+	struct gg_stem *newhand;
+	int errcode = GGI_EUNKNOWN;
 
 	for (origins = ori->first; origins <= ori->last; origins++) {
-		errcode = giiSplitInputs(*input, &newhand, origins, 0);
-		switch (errcode) {
-		case 0:
-			giiClose(newhand, GII_EV_TARGET_ALL);
-			haveone = 1;
-			break;
-		case 1:
-			giiClose(*input, GII_EV_TARGET_ALL);
-			*input = newhand;
-			haveone = 1;
-			break;
-		default:
-			// fprintf(stderr,"ERROR: %d\n",errcode);
-			break;
-		}
+		newhand = ggNewStem(libgii, NULL);
+		errcode = giiTransfer(input, newhand, origins);
+		ggDelStem(newhand);
 	}
-	if (islast) {
-		if (haveone) {
-			printfailure("last haveone expected return value: 0\n"
-				"actual return value: !=0\n");
-			return -1;
-		}
-		giiClose(*input, GII_EV_TARGET_ALL);
-	} else {
-		if (!haveone) {
-			printfailure("haveone expected return value: !=0\n"
-				"actual return value: 0\n");
-			return -1;
-		}
+	if (errcode != GGI_OK) {
+		printfailure("last haveone expected return value: \"GGI_OK\"\n"
+			"actual return value: \"%i\"\n");
+		return -1;
 	}
+
 	ggiClose(*visual);
+	ggDelStem(*visual);
 	return 0;
 }
 
@@ -186,18 +157,20 @@ static void testcase1(const char *desc)
 {
 	ggi_visual_t vis1,vis2;
 	ggi_originbounds_t oris1,oris2;
-	gii_input joinedinps;
+	struct gg_stem *joinedinps;
 
 	printteststart(__FILE__, __PRETTY_FUNCTION__, EXPECTED2PASS, desc);
 	if (dontrun) return;
 
-	joinedinps = NULL;
-	
-	if (ggi_opentestvis(&vis1, &oris1, &joinedinps, "vis1")) return;
-	if (ggi_opentestvis(&vis2, &oris2, &joinedinps, "vis2")) return;
+	joinedinps = ggNewStem(libgii, NULL);
 
-	if (ggi_closetestvis(&vis1, &oris1, &joinedinps, "vis1", 0)) return;
-	if (ggi_closetestvis(&vis2, &oris2, &joinedinps, "vis2", 1)) return;
+	if (ggi_opentestvis(&vis1, &oris1, joinedinps, "vis1")) return;
+	if (ggi_opentestvis(&vis2, &oris2, joinedinps, "vis2")) return;
+
+	if (ggi_closetestvis(&vis1, &oris1, joinedinps, "vis1", 0)) return;
+	if (ggi_closetestvis(&vis2, &oris2, joinedinps, "vis2", 1)) return;
+
+	ggDelStem(joinedinps);
 
 	printsuccess();
 	return;
@@ -207,18 +180,20 @@ static void testcase2(const char *desc)
 {
 	ggi_visual_t vis1,vis2;
 	ggi_originbounds_t oris1,oris2;
-	gii_input joinedinps;
+	struct gg_stem *joinedinps;
 
 	printteststart(__FILE__, __PRETTY_FUNCTION__, EXPECTED2PASS, desc);
 	if (dontrun) return;
 
-	joinedinps = NULL;
-	
-	if (ggi_opentestvis(&vis1, &oris1, &joinedinps, "vis1")) return;
-	if (ggi_opentestvis(&vis2, &oris2, &joinedinps, "vis2")) return;
+	joinedinps = ggNewStem(libgii, NULL);
 
-	if (ggi_closetestvis(&vis2, &oris2, &joinedinps, "vis2", 0)) return;
-	if (ggi_closetestvis(&vis1, &oris1, &joinedinps, "vis1", 1)) return;
+	if (ggi_opentestvis(&vis1, &oris1, joinedinps, "vis1")) return;
+	if (ggi_opentestvis(&vis2, &oris2, joinedinps, "vis2")) return;
+
+	if (ggi_closetestvis(&vis2, &oris2, joinedinps, "vis2", 0)) return;
+	if (ggi_closetestvis(&vis1, &oris1, joinedinps, "vis1", 1)) return;
+
+	ggDelStem(joinedinps);
 
 	printsuccess();
 	return;
