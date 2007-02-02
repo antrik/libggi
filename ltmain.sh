71b8c591dc16b2262bb20a7163dd3cfda3a5545b
cegger <cegger>
2007-02-02 20:13:19 +0000

    sync with libtool head version to 1.2373

--- ltmain.sh
+++ ltmain.sh
@@ -1,6 +1,6 @@
 # Generated from ltmain.m4sh; do not edit by hand
 
-# ltmain.sh (GNU libtool 1.2362 2007/01/23 23:48:55) 2.1a
+# ltmain.sh (GNU libtool 1.2373 2007/01/29 21:59:58) 2.1a
 # Written by Gordon Matzigkeit <gord@gnu.ai.mit.edu>, 1996
 
 # Copyright (C) 1996, 1997, 1998, 1999, 2000, 2001, 2003, 2004, 2005, 2006, 2007 Free Software Foundation, Inc.
@@ -63,7 +63,7 @@
 #       compiler:		$LTCC
 #       compiler flags:		$LTCFLAGS
 #       linker:		$LD (gnu? $with_gnu_ld)
-#       $progname:		(GNU libtool 1.2362 2007/01/23 23:48:55) 2.1a
+#       $progname:		(GNU libtool 1.2373 2007/01/29 21:59:58) 2.1a
 #       automake:		$automake_version
 #       autoconf:		$autoconf_version
 #
@@ -72,8 +72,8 @@
 PROGRAM=ltmain.sh
 PACKAGE=libtool
 VERSION=2.1a
-TIMESTAMP=" 1.2362 2007/01/23 23:48:55"
-package_revision=1.2362
+TIMESTAMP=" 1.2373 2007/01/29 21:59:58"
+package_revision=1.2373
 
 # Be Bourne compatible
 if test -n "${ZSH_VERSION+set}" && (emulate sh) >/dev/null 2>&1; then
@@ -3269,10 +3269,10 @@ func_mode_link ()
       # -m*, -t[45]*, -txscale* pass through architecture-specific
       # compiler args for GCC
       # -F/path gives path to uninstalled frameworks, gcc on darwin
-      # -pg, --coverage pass through profiling flag for GCC
+      # -p, -pg, --coverage, -fprofile-* pass through profiling flag for GCC
       # @file GCC response files
       -64|-mips[0-9]|-r[0-9][0-9]*|-xarch=*|-xtarget=*|+DA*|+DD*|-q*|-m*| \
-      -t[45]*|-txscale*|-pg|--coverage|-F*|@*)
+      -t[45]*|-txscale*|-p|-pg|--coverage|-fprofile-*|-F*|@*)
         func_quote_for_eval "$arg"
 	arg="$func_quote_for_eval_result"
         func_append compile_command " $arg"
@@ -4428,6 +4428,10 @@ func_mode_link ()
 		      depdepl="$absdir/$objdir/$depdepl"
 		      darwin_install_name=`otool -L $depdepl | $SED -n -e '3q;2,2p' | $SED -e 's/(.*//'`
 		      darwin_install_name=`$ECHO $darwin_install_name`
+                      if test -z "$darwin_install_name"; then
+                          darwin_install_name=`otool64 -L $depdepl | $SED -n -e '3q;2,2p' | $SED -e 's/(.*//'`
+                          darwin_install_name=`$ECHO $darwin_install_name`
+                      fi
 		      compiler_flags="$compiler_flags ${wl}-dylib_file ${wl}${darwin_install_name}:${depdepl}"
 		      linker_flags="$linker_flags -dylib_file ${darwin_install_name}:${depdepl}"
 		      path=
