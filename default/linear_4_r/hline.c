85b75dc5c3d14d32ecf54ca1fc41e58292f05328
pekberg <pekberg>
2007-04-05 12:44:39 +0000

    Fix clipping of packed puthline for the case where an odd number of pixels are clipped to the left. Fixes problems revealed by the "get" regression test.

--- default/linear_4_r/hline.c
+++ default/linear_4_r/hline.c
@@ -1,4 +1,4 @@
-/* $Id: hline.c,v 1.11 2007/04/04 20:07:30 ggibecka Exp $
+/* $Id: hline.c,v 1.12 2007/04/05 12:44:39 pekberg Exp $
 ******************************************************************************
 
    Graphics library for GGI. Horizontal lines.
@@ -84,21 +84,49 @@ int GGI_lin4r_packed_puthline(struct ggi_visual *vis, int x, int y, int w, const
 	uint8_t *fb;
 	uint16_t color;
 	const uint8_t *buf = (const uint8_t *)buffer;
+	int diff = 0;
 
-	LIBGGICLIP_XYW_BUFMOD(vis, x, y, w, buf, /2);
+	if (y < LIBGGI_GC(vis)->cliptl.y ||
+	    y >= LIBGGI_GC(vis)->clipbr.y) {
+		return 0;
+	}
+	if (x < LIBGGI_GC(vis)->cliptl.x) {
+		diff = LIBGGI_GC(vis)->cliptl.x - x;
+		x   += diff;
+		buf += diff >> 1;
+		w -= diff;
+		diff &= 1;
+	}
+	if (x+w > LIBGGI_GC(vis)->clipbr.x) {
+		w = LIBGGI_GC(vis)->clipbr.x - x;
+	}
+	if (w <= 0) return 0;
 	PREPARE_FB(vis);
 	
 	fb = (uint8_t*)LIBGGI_CURWRITE(vis)+y*LIBGGI_FB_W_STRIDE(vis)+(x/2);
 	
 	/* Might as well take the memcpy speed up (multiple byte copy). */
-	if (!(x & 0x01)) {
-		memcpy(fb,buf, (size_t)(w/2));
-		if (w & 0x01)
-			*(fb+(w/2)) = ( *(fb+(w/2)) & 0xF0)
-				| (*(buf+(w/2)) >> 4);
+	if (!((diff + x) & 1)) {
+		if (diff & 1) {
+			// x and diff both odd, fixup initial pixel */
+			*fb &= 0x0f;
+			*fb++ |= *buf++ & 0xf0;
+			if (!--w) return 0;
+		}
+		memcpy(fb, buf, (size_t)(w>>1));
+		if (w & 1)
+			*(fb+(w>>1)) = ( *(fb+(w>>1)) & 0xF0)
+				| (*(buf+(w>>1)) >> 4);
 		return 0;
 	}
 	
+	if (diff & 1) {
+		/* diff-odd, x-even -> unite with the diff-even, x-odd case */
+		*fb &= 0xf0;
+		*fb |= *buf++ >> 4;
+		if (!--w) return 0;
+	}
+
 	/* x is odd. */
 	/* Could be optimized for multiple-byte copy. */
 
