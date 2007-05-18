1742f958c464e4cfcf5f2b81499c6b3ee8029efe
pekberg <pekberg>
2007-05-18 21:56:09 +0000

    Make the linear-32 default renderer (e.g 104x100[C32]) pass the getvline test in regress/get.

--- default/linear_32/vline.c
+++ default/linear_32/vline.c
@@ -1,4 +1,4 @@
-/* $Id: vline.c,v 1.5 2006/03/12 23:15:08 soyt Exp $
+/* $Id: vline.c,v 1.6 2007/05/18 21:56:09 pekberg Exp $
 ******************************************************************************
 
    Graphics library for GGI.
@@ -81,13 +81,25 @@ int GGI_lin32_putvline(struct ggi_visual *vis, int x, int y, int h, const void *
 
 int GGI_lin32_getvline(struct ggi_visual *vis, int x, int y, int h, void *buffer)
 {
-	uint32_t *ptr, *buf32;
+	uint32_t *ptr, *buf32 = buffer;
 	int stride = LIBGGI_FB_R_STRIDE(vis)/4;
+
+	if (x < 0 || x >= LIBGGI_VIRTX(vis))
+		return 0;
+
+	if (y < 0) {
+		h += y;
+		buf32 -= y;
+		y = 0;
+	}
+	if (y + h > LIBGGI_VIRTY(vis))
+		h = LIBGGI_VIRTY(vis) - y;
+	if (h <= 0)
+		return 0;
 
 	PREPARE_FB(vis);
 
 	ptr = ((uint32_t *)LIBGGI_CURREAD(vis)) + y*stride + x;
-	buf32  = buffer;
 
 	for (; h > 0; h--, ptr += stride) {
 		*(buf32++) = *ptr; 
