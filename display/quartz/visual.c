f941841f704f8c55b9415bd607887262cbb7df26
cegger <cegger>
2007-06-20 06:52:09 +0000

    Enable new style module loading/unloading and remove old style code. With this, helper-mansync unloads on exit again.

--- display/quartz/visual.c
+++ display/quartz/visual.c
@@ -1,4 +1,4 @@
-/* $Id: visual.c,v 1.29 2007/06/06 18:58:04 cegger Exp $
+/* $Id: visual.c,v 1.30 2007/06/20 06:52:09 cegger Exp $
 ******************************************************************************
 
    Display-quartz: initialization
@@ -159,15 +159,6 @@ static int GGIopen(struct ggi_visual *vis, struct ggi_dlhandle *dlh,
 		}	/* if */
 
 		DPRINT_LIBS("loading helper-mansync\n");
-#if 1
-		err = _ggiAddDL(vis, _ggiGetConfigHandle(),
-				"helper-mansync", NULL, priv->opmansync, 0);
-		if (err != GGI_OK) {
-			fprintf(stderr,
-				"display-quartz: Cannot load requrired helper-mansync\n");
-			goto out;
-		}	/* if */
-#else
 		MANSYNC_open(vis, priv);
 		if (priv->mod_mansync == NULL) {
 			DPRINT_LIBS("loading helper-mansync failed\n");
@@ -176,7 +167,6 @@ static int GGIopen(struct ggi_visual *vis, struct ggi_dlhandle *dlh,
 			err = GGI_ENODEVICE;
 			goto out;
 		}
-#endif
 	}	/* if */
 
 	/* windowed mode is default */
@@ -324,9 +314,7 @@ static int GGIexit(struct ggi_visual *vis, struct ggi_dlhandle *dlh)
 			MANSYNC_stop(vis);
 		}
 		MANSYNC_deinit(vis);
-#if 0
 		MANSYNC_close(priv);
-#endif
 	}
 
 	return 0;
