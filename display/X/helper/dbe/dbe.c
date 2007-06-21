39219fa6f7c8ce2a5dece34c7f57b09f8cc89ff8
cegger <cegger>
2007-06-21 07:29:08 +0000

    make helper-x-dbe a hybrid of old style and new style module.

--- display/X/helper/dbe/dbe.c
+++ display/X/helper/dbe/dbe.c
@@ -1,4 +1,4 @@
-/* $Id: dbe.c,v 1.16 2007/05/07 21:35:31 mooz Exp $
+/* $Id: dbe.c,v 1.17 2007/06/21 07:29:08 cegger Exp $
 ******************************************************************************
 
    DBE extension support for display-x
@@ -30,6 +30,7 @@
 #include <ggi/internal/ggi_debug.h>
 #include <ggi/display/x.h>
 #include <X11/extensions/Xdbe.h>
+#include <ggi/internal/ggi-module.h>
 
 #if 0
 static void GGI_DBE_swap(struct ggi_visual *vis)
@@ -105,6 +106,50 @@ static int GGIclose(struct ggi_visual *vis, struct ggi_dlhandle *dlh)
 }
 
 
+static int
+GGI_helper_x_dbe_setup(struct ggi_helper *helper,
+			const char *args, void *argptr)
+{
+	ggi_x_priv *priv = GGIX_PRIV(helper->visual);
+	int major_version, minor_version;
+	Status rc;
+
+	/* Check if DBE is present before initialising */
+	rc = XdbeQueryExtension(priv->disp, &major_version, &minor_version);
+	if (rc != True) return GGI_ENOFUNC;
+
+	DPRINT_LIBS("X: DOUBLE-BUFFER: DBE version %i.%i\n",
+			major_version, minor_version);
+
+	return GGI_OK;
+}
+
+static void
+GGI_helper_x_dbe_teardown(struct ggi_helper *helper)
+{
+#if 0
+	ggi_x_priv *priv = GGIX_PRIV(helper->visual);
+	XdbeBackBuffer backBuffer;
+
+	/* Deallocate back buffer */
+	if(!XdbeDeallocateBackBufferName(priv->disp, priv->drawable)) {
+		DPRINT_LIBS("X: DOUBLE-BUFFER: Unable to deallocate back buffer.\n");
+		return GGI_EFATAL;
+	}
+#endif
+}
+
+struct ggi_module_helper GGI_helper_x_dbe = {
+	GG_MODULE_INIT("helper-x-dbe", 0, 1, GGI_MODULE_HELPER),
+	GGI_helper_x_dbe_setup,
+	GGI_helper_x_dbe_teardown
+};
+
+static struct ggi_module_helper *_GGIdl_helper_x_dbe[] = {
+	&GGI_helper_x_dbe,
+	NULL
+};
+
 EXPORTFUNC
 int GGIdl_helper_x_dbe(int func, void **funcptr);
 
@@ -112,6 +157,7 @@ int GGIdl_helper_x_dbe(int func, void **funcptr)
 {
 	ggifunc_open **openptr;
 	ggifunc_close **closeptr;
+	struct ggi_module_helper ***modulesptr;
 
 	switch (func) {
 	case GGIFUNC_open:
@@ -125,6 +171,10 @@ int GGIdl_helper_x_dbe(int func, void **funcptr)
 		closeptr = (ggifunc_close **)funcptr;
 		*closeptr = GGIclose;
 		return 0;
+	case GG_DLENTRY_MODULES:
+		modulesptr = (struct ggi_module_helper ***)funcptr;
+		*modulesptr = _GGIdl_helper_x_dbe;
+		return GGI_OK;
 	default:
 		*funcptr = NULL;
 	}
