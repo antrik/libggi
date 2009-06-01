9824f269a67f25fe9899357ac60965099484b945
antrik <antrik@users.sf.net>
2009-06-01 06:48:30 +0200

    Add kgiUser.[ch] for Hurd
    These generated files are simply included statically in the source tree
    for now. Will have to find a way to generate them from kgi.defs later
    on.

---
+++ display/kgi/kgiUser.c
@@ -0,0 +1,770 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE 1
+#endif
+
+#include "kgiUser.h"
+#define EXPORT_BOOLEAN
+#include <mach/boolean.h>
+#include <mach/kern_return.h>
+#include <mach/message.h>
+#include <mach/notify.h>
+#include <mach/mach_types.h>
+#include <mach/mig_errors.h>
+#include <mach/mig_support.h>
+#include <mach/msg_type.h>
+/* LINTLIBRARY */
+
+#ifndef	mig_internal
+#define	mig_internal	static
+#endif
+
+#ifndef	mig_external
+#define mig_external
+#endif
+
+#ifndef	TypeCheck
+#define	TypeCheck 1
+#endif
+
+#ifndef	UseExternRCSId
+#define	UseExternRCSId		1
+#endif
+
+#define BAD_TYPECHECK(type, check) ({\
+  union { mach_msg_type_t t; unsigned32_t w; } _t, _c;\
+  _t.t = *(type); _c.t = *(check); _t.w != _c.w; })
+#define msgh_request_port	msgh_remote_port
+#define msgh_reply_port		msgh_local_port
+
+#include <mach/std_types.h>
+#include <mach/mach_types.h>
+#include <device/device_types.h>
+#include <device/net_status.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/statfs.h>
+#include <sys/resource.h>
+#include <sys/utsname.h>
+#include <hurd/hurd_types.h>
+#include <hurd/trivfs.h>
+#include "kgi/kgi.h"
+
+/* Routine kgi_set_images */
+mig_external kern_return_t kgi_set_images
+(
+	io_t io_object,
+	int images
+)
+{
+	typedef struct {
+		mach_msg_header_t Head;
+		mach_msg_type_t imagesType;
+		int images;
+	} Request;
+
+	typedef struct {
+		mach_msg_header_t Head;
+		mach_msg_type_t RetCodeType;
+		kern_return_t RetCode;
+	} Reply;
+
+	union {
+		Request In;
+		Reply Out;
+	} Mess;
+
+	register Request *InP = &Mess.In;
+	register Reply *OutP = &Mess.Out;
+
+	mach_msg_return_t msg_result;
+
+	auto const mach_msg_type_t imagesType = {
+		/* msgt_name = */		2,
+		/* msgt_size = */		32,
+		/* msgt_number = */		1,
+		/* msgt_inline = */		TRUE,
+		/* msgt_longform = */		FALSE,
+		/* msgt_deallocate = */		FALSE,
+		/* msgt_unused = */		0
+	};
+
+	auto const mach_msg_type_t RetCodeCheck = {
+		/* msgt_name = */		MACH_MSG_TYPE_INTEGER_32,
+		/* msgt_size = */		32,
+		/* msgt_number = */		1,
+		/* msgt_inline = */		TRUE,
+		/* msgt_longform = */		FALSE,
+		/* msgt_deallocate = */		FALSE,
+		/* msgt_unused = */		0
+	};
+
+	InP->imagesType = imagesType;
+
+	InP->images = images;
+
+	InP->Head.msgh_bits =
+		MACH_MSGH_BITS(19, MACH_MSG_TYPE_MAKE_SEND_ONCE);
+	/* msgh_size passed as argument */
+	InP->Head.msgh_request_port = io_object;
+	InP->Head.msgh_reply_port = mig_get_reply_port();
+	InP->Head.msgh_seqno = 0;
+	InP->Head.msgh_id = 99000;
+
+	msg_result = mach_msg(&InP->Head, MACH_SEND_MSG|MACH_RCV_MSG|MACH_MSG_OPTION_NONE, 32, sizeof(Reply), InP->Head.msgh_reply_port, MACH_MSG_TIMEOUT_NONE, MACH_PORT_NULL);
+	if (msg_result != MACH_MSG_SUCCESS) {
+		mig_dealloc_reply_port(InP->Head.msgh_reply_port);
+		return msg_result;
+	}
+	mig_put_reply_port(InP->Head.msgh_reply_port);
+
+	if (OutP->Head.msgh_id != 99100) {
+		if (OutP->Head.msgh_id == MACH_NOTIFY_SEND_ONCE)
+		return MIG_SERVER_DIED;
+		else {
+			mig_dealloc_reply_port(InP->Head.msgh_reply_port);
+			return MIG_REPLY_MISMATCH;
+		}
+	}
+
+#if	TypeCheck
+	if ((OutP->Head.msgh_bits & MACH_MSGH_BITS_COMPLEX) ||
+	    (OutP->Head.msgh_size != 32))
+		return MIG_TYPE_ERROR;
+#endif	/* TypeCheck */
+
+#if	TypeCheck
+	if (BAD_TYPECHECK (&OutP->RetCodeType, &RetCodeCheck))
+		return MIG_TYPE_ERROR;
+#endif	/* TypeCheck */
+
+	return OutP->RetCode;
+}
+
+/* Routine kgi_set_image_mode */
+mig_external kern_return_t kgi_set_image_mode
+(
+	io_t io_object,
+	int image,
+	kgi_image_mode_t mode
+)
+{
+	typedef struct {
+		mach_msg_header_t Head;
+		mach_msg_type_t imageType;
+		int image;
+		mach_msg_type_t modeType;
+		kgi_image_mode_t mode;
+	} Request;
+
+	typedef struct {
+		mach_msg_header_t Head;
+		mach_msg_type_t RetCodeType;
+		kern_return_t RetCode;
+	} Reply;
+
+	union {
+		Request In;
+		Reply Out;
+	} Mess;
+
+	register Request *InP = &Mess.In;
+	register Reply *OutP = &Mess.Out;
+
+	mach_msg_return_t msg_result;
+
+	auto const mach_msg_type_t imageType = {
+		/* msgt_name = */		2,
+		/* msgt_size = */		32,
+		/* msgt_number = */		1,
+		/* msgt_inline = */		TRUE,
+		/* msgt_longform = */		FALSE,
+		/* msgt_deallocate = */		FALSE,
+		/* msgt_unused = */		0
+	};
+
+	auto const mach_msg_type_t modeType = {
+		/* msgt_name = */		8,
+		/* msgt_size = */		8,
+		/* msgt_number = */		172,
+		/* msgt_inline = */		TRUE,
+		/* msgt_longform = */		FALSE,
+		/* msgt_deallocate = */		FALSE,
+		/* msgt_unused = */		0
+	};
+
+	auto const mach_msg_type_t RetCodeCheck = {
+		/* msgt_name = */		MACH_MSG_TYPE_INTEGER_32,
+		/* msgt_size = */		32,
+		/* msgt_number = */		1,
+		/* msgt_inline = */		TRUE,
+		/* msgt_longform = */		FALSE,
+		/* msgt_deallocate = */		FALSE,
+		/* msgt_unused = */		0
+	};
+
+	InP->imageType = imageType;
+
+	InP->image = image;
+
+	InP->modeType = modeType;
+
+	InP->mode = mode;
+
+	InP->Head.msgh_bits =
+		MACH_MSGH_BITS(19, MACH_MSG_TYPE_MAKE_SEND_ONCE);
+	/* msgh_size passed as argument */
+	InP->Head.msgh_request_port = io_object;
+	InP->Head.msgh_reply_port = mig_get_reply_port();
+	InP->Head.msgh_seqno = 0;
+	InP->Head.msgh_id = 99001;
+
+	msg_result = mach_msg(&InP->Head, MACH_SEND_MSG|MACH_RCV_MSG|MACH_MSG_OPTION_NONE, 208, sizeof(Reply), InP->Head.msgh_reply_port, MACH_MSG_TIMEOUT_NONE, MACH_PORT_NULL);
+	if (msg_result != MACH_MSG_SUCCESS) {
+		mig_dealloc_reply_port(InP->Head.msgh_reply_port);
+		return msg_result;
+	}
+	mig_put_reply_port(InP->Head.msgh_reply_port);
+
+	if (OutP->Head.msgh_id != 99101) {
+		if (OutP->Head.msgh_id == MACH_NOTIFY_SEND_ONCE)
+		return MIG_SERVER_DIED;
+		else {
+			mig_dealloc_reply_port(InP->Head.msgh_reply_port);
+			return MIG_REPLY_MISMATCH;
+		}
+	}
+
+#if	TypeCheck
+	if ((OutP->Head.msgh_bits & MACH_MSGH_BITS_COMPLEX) ||
+	    (OutP->Head.msgh_size != 32))
+		return MIG_TYPE_ERROR;
+#endif	/* TypeCheck */
+
+#if	TypeCheck
+	if (BAD_TYPECHECK (&OutP->RetCodeType, &RetCodeCheck))
+		return MIG_TYPE_ERROR;
+#endif	/* TypeCheck */
+
+	return OutP->RetCode;
+}
+
+/* Routine kgi_get_image_mode */
+mig_external kern_return_t kgi_get_image_mode
+(
+	io_t io_object,
+	int image,
+	kgi_image_mode_t *mode
+)
+{
+	typedef struct {
+		mach_msg_header_t Head;
+		mach_msg_type_t imageType;
+		int image;
+	} Request;
+
+	typedef struct {
+		mach_msg_header_t Head;
+		mach_msg_type_t RetCodeType;
+		kern_return_t RetCode;
+		mach_msg_type_t modeType;
+		kgi_image_mode_t mode;
+	} Reply;
+
+	union {
+		Request In;
+		Reply Out;
+	} Mess;
+
+	register Request *InP = &Mess.In;
+	register Reply *OutP = &Mess.Out;
+
+	mach_msg_return_t msg_result;
+#if	TypeCheck
+	unsigned int msgh_size;
+#endif	/* TypeCheck */
+
+	auto const mach_msg_type_t imageType = {
+		/* msgt_name = */		2,
+		/* msgt_size = */		32,
+		/* msgt_number = */		1,
+		/* msgt_inline = */		TRUE,
+		/* msgt_longform = */		FALSE,
+		/* msgt_deallocate = */		FALSE,
+		/* msgt_unused = */		0
+	};
+
+	auto const mach_msg_type_t RetCodeCheck = {
+		/* msgt_name = */		MACH_MSG_TYPE_INTEGER_32,
+		/* msgt_size = */		32,
+		/* msgt_number = */		1,
+		/* msgt_inline = */		TRUE,
+		/* msgt_longform = */		FALSE,
+		/* msgt_deallocate = */		FALSE,
+		/* msgt_unused = */		0
+	};
+
+	auto const mach_msg_type_t modeCheck = {
+		/* msgt_name = */		8,
+		/* msgt_size = */		8,
+		/* msgt_number = */		172,
+		/* msgt_inline = */		TRUE,
+		/* msgt_longform = */		FALSE,
+		/* msgt_deallocate = */		FALSE,
+		/* msgt_unused = */		0
+	};
+
+	InP->imageType = imageType;
+
+	InP->image = image;
+
+	InP->Head.msgh_bits =
+		MACH_MSGH_BITS(19, MACH_MSG_TYPE_MAKE_SEND_ONCE);
+	/* msgh_size passed as argument */
+	InP->Head.msgh_request_port = io_object;
+	InP->Head.msgh_reply_port = mig_get_reply_port();
+	InP->Head.msgh_seqno = 0;
+	InP->Head.msgh_id = 99002;
+
+	msg_result = mach_msg(&InP->Head, MACH_SEND_MSG|MACH_RCV_MSG|MACH_MSG_OPTION_NONE, 32, sizeof(Reply), InP->Head.msgh_reply_port, MACH_MSG_TIMEOUT_NONE, MACH_PORT_NULL);
+	if (msg_result != MACH_MSG_SUCCESS) {
+		mig_dealloc_reply_port(InP->Head.msgh_reply_port);
+		return msg_result;
+	}
+	mig_put_reply_port(InP->Head.msgh_reply_port);
+
+	if (OutP->Head.msgh_id != 99102) {
+		if (OutP->Head.msgh_id == MACH_NOTIFY_SEND_ONCE)
+		return MIG_SERVER_DIED;
+		else {
+			mig_dealloc_reply_port(InP->Head.msgh_reply_port);
+			return MIG_REPLY_MISMATCH;
+		}
+	}
+
+#if	TypeCheck
+	msgh_size = OutP->Head.msgh_size;
+
+	if ((OutP->Head.msgh_bits & MACH_MSGH_BITS_COMPLEX) ||
+	    ((msgh_size != 208) &&
+	     ((msgh_size != sizeof(mig_reply_header_t)) ||
+	      (OutP->RetCode == KERN_SUCCESS))))
+		return MIG_TYPE_ERROR;
+#endif	/* TypeCheck */
+
+#if	TypeCheck
+	if (BAD_TYPECHECK (&OutP->RetCodeType, &RetCodeCheck))
+		return MIG_TYPE_ERROR;
+#endif	/* TypeCheck */
+
+	if (OutP->RetCode != KERN_SUCCESS)
+		return OutP->RetCode;
+
+#if	TypeCheck
+	if (BAD_TYPECHECK (&OutP->modeType, &modeCheck))
+		return MIG_TYPE_ERROR;
+#endif	/* TypeCheck */
+
+	*mode = OutP->mode;
+
+	return KERN_SUCCESS;
+}
+
+/* Routine kgi_check_mode */
+mig_external kern_return_t kgi_check_mode
+(
+	io_t io_object
+)
+{
+	typedef struct {
+		mach_msg_header_t Head;
+	} Request;
+
+	typedef struct {
+		mach_msg_header_t Head;
+		mach_msg_type_t RetCodeType;
+		kern_return_t RetCode;
+	} Reply;
+
+	union {
+		Request In;
+		Reply Out;
+	} Mess;
+
+	register Request *InP = &Mess.In;
+	register Reply *OutP = &Mess.Out;
+
+	mach_msg_return_t msg_result;
+
+	auto const mach_msg_type_t RetCodeCheck = {
+		/* msgt_name = */		MACH_MSG_TYPE_INTEGER_32,
+		/* msgt_size = */		32,
+		/* msgt_number = */		1,
+		/* msgt_inline = */		TRUE,
+		/* msgt_longform = */		FALSE,
+		/* msgt_deallocate = */		FALSE,
+		/* msgt_unused = */		0
+	};
+
+	InP->Head.msgh_bits =
+		MACH_MSGH_BITS(19, MACH_MSG_TYPE_MAKE_SEND_ONCE);
+	/* msgh_size passed as argument */
+	InP->Head.msgh_request_port = io_object;
+	InP->Head.msgh_reply_port = mig_get_reply_port();
+	InP->Head.msgh_seqno = 0;
+	InP->Head.msgh_id = 99003;
+
+	msg_result = mach_msg(&InP->Head, MACH_SEND_MSG|MACH_RCV_MSG|MACH_MSG_OPTION_NONE, 24, sizeof(Reply), InP->Head.msgh_reply_port, MACH_MSG_TIMEOUT_NONE, MACH_PORT_NULL);
+	if (msg_result != MACH_MSG_SUCCESS) {
+		mig_dealloc_reply_port(InP->Head.msgh_reply_port);
+		return msg_result;
+	}
+	mig_put_reply_port(InP->Head.msgh_reply_port);
+
+	if (OutP->Head.msgh_id != 99103) {
+		if (OutP->Head.msgh_id == MACH_NOTIFY_SEND_ONCE)
+		return MIG_SERVER_DIED;
+		else {
+			mig_dealloc_reply_port(InP->Head.msgh_reply_port);
+			return MIG_REPLY_MISMATCH;
+		}
+	}
+
+#if	TypeCheck
+	if ((OutP->Head.msgh_bits & MACH_MSGH_BITS_COMPLEX) ||
+	    (OutP->Head.msgh_size != 32))
+		return MIG_TYPE_ERROR;
+#endif	/* TypeCheck */
+
+#if	TypeCheck
+	if (BAD_TYPECHECK (&OutP->RetCodeType, &RetCodeCheck))
+		return MIG_TYPE_ERROR;
+#endif	/* TypeCheck */
+
+	return OutP->RetCode;
+}
+
+/* Routine kgi_set_mode */
+mig_external kern_return_t kgi_set_mode
+(
+	io_t io_object
+)
+{
+	typedef struct {
+		mach_msg_header_t Head;
+	} Request;
+
+	typedef struct {
+		mach_msg_header_t Head;
+		mach_msg_type_t RetCodeType;
+		kern_return_t RetCode;
+	} Reply;
+
+	union {
+		Request In;
+		Reply Out;
+	} Mess;
+
+	register Request *InP = &Mess.In;
+	register Reply *OutP = &Mess.Out;
+
+	mach_msg_return_t msg_result;
+
+	auto const mach_msg_type_t RetCodeCheck = {
+		/* msgt_name = */		MACH_MSG_TYPE_INTEGER_32,
+		/* msgt_size = */		32,
+		/* msgt_number = */		1,
+		/* msgt_inline = */		TRUE,
+		/* msgt_longform = */		FALSE,
+		/* msgt_deallocate = */		FALSE,
+		/* msgt_unused = */		0
+	};
+
+	InP->Head.msgh_bits =
+		MACH_MSGH_BITS(19, MACH_MSG_TYPE_MAKE_SEND_ONCE);
+	/* msgh_size passed as argument */
+	InP->Head.msgh_request_port = io_object;
+	InP->Head.msgh_reply_port = mig_get_reply_port();
+	InP->Head.msgh_seqno = 0;
+	InP->Head.msgh_id = 99004;
+
+	msg_result = mach_msg(&InP->Head, MACH_SEND_MSG|MACH_RCV_MSG|MACH_MSG_OPTION_NONE, 24, sizeof(Reply), InP->Head.msgh_reply_port, MACH_MSG_TIMEOUT_NONE, MACH_PORT_NULL);
+	if (msg_result != MACH_MSG_SUCCESS) {
+		mig_dealloc_reply_port(InP->Head.msgh_reply_port);
+		return msg_result;
+	}
+	mig_put_reply_port(InP->Head.msgh_reply_port);
+
+	if (OutP->Head.msgh_id != 99104) {
+		if (OutP->Head.msgh_id == MACH_NOTIFY_SEND_ONCE)
+		return MIG_SERVER_DIED;
+		else {
+			mig_dealloc_reply_port(InP->Head.msgh_reply_port);
+			return MIG_REPLY_MISMATCH;
+		}
+	}
+
+#if	TypeCheck
+	if ((OutP->Head.msgh_bits & MACH_MSGH_BITS_COMPLEX) ||
+	    (OutP->Head.msgh_size != 32))
+		return MIG_TYPE_ERROR;
+#endif	/* TypeCheck */
+
+#if	TypeCheck
+	if (BAD_TYPECHECK (&OutP->RetCodeType, &RetCodeCheck))
+		return MIG_TYPE_ERROR;
+#endif	/* TypeCheck */
+
+	return OutP->RetCode;
+}
+
+/* Routine kgi_unset_mode */
+mig_external kern_return_t kgi_unset_mode
+(
+	io_t io_object
+)
+{
+	typedef struct {
+		mach_msg_header_t Head;
+	} Request;
+
+	typedef struct {
+		mach_msg_header_t Head;
+		mach_msg_type_t RetCodeType;
+		kern_return_t RetCode;
+	} Reply;
+
+	union {
+		Request In;
+		Reply Out;
+	} Mess;
+
+	register Request *InP = &Mess.In;
+	register Reply *OutP = &Mess.Out;
+
+	mach_msg_return_t msg_result;
+
+	auto const mach_msg_type_t RetCodeCheck = {
+		/* msgt_name = */		MACH_MSG_TYPE_INTEGER_32,
+		/* msgt_size = */		32,
+		/* msgt_number = */		1,
+		/* msgt_inline = */		TRUE,
+		/* msgt_longform = */		FALSE,
+		/* msgt_deallocate = */		FALSE,
+		/* msgt_unused = */		0
+	};
+
+	InP->Head.msgh_bits =
+		MACH_MSGH_BITS(19, MACH_MSG_TYPE_MAKE_SEND_ONCE);
+	/* msgh_size passed as argument */
+	InP->Head.msgh_request_port = io_object;
+	InP->Head.msgh_reply_port = mig_get_reply_port();
+	InP->Head.msgh_seqno = 0;
+	InP->Head.msgh_id = 99005;
+
+	msg_result = mach_msg(&InP->Head, MACH_SEND_MSG|MACH_RCV_MSG|MACH_MSG_OPTION_NONE, 24, sizeof(Reply), InP->Head.msgh_reply_port, MACH_MSG_TIMEOUT_NONE, MACH_PORT_NULL);
+	if (msg_result != MACH_MSG_SUCCESS) {
+		mig_dealloc_reply_port(InP->Head.msgh_reply_port);
+		return msg_result;
+	}
+	mig_put_reply_port(InP->Head.msgh_reply_port);
+
+	if (OutP->Head.msgh_id != 99105) {
+		if (OutP->Head.msgh_id == MACH_NOTIFY_SEND_ONCE)
+		return MIG_SERVER_DIED;
+		else {
+			mig_dealloc_reply_port(InP->Head.msgh_reply_port);
+			return MIG_REPLY_MISMATCH;
+		}
+	}
+
+#if	TypeCheck
+	if ((OutP->Head.msgh_bits & MACH_MSGH_BITS_COMPLEX) ||
+	    (OutP->Head.msgh_size != 32))
+		return MIG_TYPE_ERROR;
+#endif	/* TypeCheck */
+
+#if	TypeCheck
+	if (BAD_TYPECHECK (&OutP->RetCodeType, &RetCodeCheck))
+		return MIG_TYPE_ERROR;
+#endif	/* TypeCheck */
+
+	return OutP->RetCode;
+}
+
+/* Routine kgi_get_fb_resource */
+mig_external kern_return_t kgi_get_fb_resource
+(
+	io_t io_object,
+	kgic_mapper_resource_info_result_t *fb
+)
+{
+	typedef struct {
+		mach_msg_header_t Head;
+	} Request;
+
+	typedef struct {
+		mach_msg_header_t Head;
+		mach_msg_type_t RetCodeType;
+		kern_return_t RetCode;
+		mach_msg_type_t fbType;
+		kgic_mapper_resource_info_result_t fb;
+	} Reply;
+
+	union {
+		Request In;
+		Reply Out;
+	} Mess;
+
+	register Request *InP = &Mess.In;
+	register Reply *OutP = &Mess.Out;
+
+	mach_msg_return_t msg_result;
+#if	TypeCheck
+	unsigned int msgh_size;
+#endif	/* TypeCheck */
+
+	auto const mach_msg_type_t RetCodeCheck = {
+		/* msgt_name = */		MACH_MSG_TYPE_INTEGER_32,
+		/* msgt_size = */		32,
+		/* msgt_number = */		1,
+		/* msgt_inline = */		TRUE,
+		/* msgt_longform = */		FALSE,
+		/* msgt_deallocate = */		FALSE,
+		/* msgt_unused = */		0
+	};
+
+	auto const mach_msg_type_t fbCheck = {
+		/* msgt_name = */		8,
+		/* msgt_size = */		8,
+		/* msgt_number = */		96,
+		/* msgt_inline = */		TRUE,
+		/* msgt_longform = */		FALSE,
+		/* msgt_deallocate = */		FALSE,
+		/* msgt_unused = */		0
+	};
+
+	InP->Head.msgh_bits =
+		MACH_MSGH_BITS(19, MACH_MSG_TYPE_MAKE_SEND_ONCE);
+	/* msgh_size passed as argument */
+	InP->Head.msgh_request_port = io_object;
+	InP->Head.msgh_reply_port = mig_get_reply_port();
+	InP->Head.msgh_seqno = 0;
+	InP->Head.msgh_id = 99006;
+
+	msg_result = mach_msg(&InP->Head, MACH_SEND_MSG|MACH_RCV_MSG|MACH_MSG_OPTION_NONE, 24, sizeof(Reply), InP->Head.msgh_reply_port, MACH_MSG_TIMEOUT_NONE, MACH_PORT_NULL);
+	if (msg_result != MACH_MSG_SUCCESS) {
+		mig_dealloc_reply_port(InP->Head.msgh_reply_port);
+		return msg_result;
+	}
+	mig_put_reply_port(InP->Head.msgh_reply_port);
+
+	if (OutP->Head.msgh_id != 99106) {
+		if (OutP->Head.msgh_id == MACH_NOTIFY_SEND_ONCE)
+		return MIG_SERVER_DIED;
+		else {
+			mig_dealloc_reply_port(InP->Head.msgh_reply_port);
+			return MIG_REPLY_MISMATCH;
+		}
+	}
+
+#if	TypeCheck
+	msgh_size = OutP->Head.msgh_size;
+
+	if ((OutP->Head.msgh_bits & MACH_MSGH_BITS_COMPLEX) ||
+	    ((msgh_size != 132) &&
+	     ((msgh_size != sizeof(mig_reply_header_t)) ||
+	      (OutP->RetCode == KERN_SUCCESS))))
+		return MIG_TYPE_ERROR;
+#endif	/* TypeCheck */
+
+#if	TypeCheck
+	if (BAD_TYPECHECK (&OutP->RetCodeType, &RetCodeCheck))
+		return MIG_TYPE_ERROR;
+#endif	/* TypeCheck */
+
+	if (OutP->RetCode != KERN_SUCCESS)
+		return OutP->RetCode;
+
+#if	TypeCheck
+	if (BAD_TYPECHECK (&OutP->fbType, &fbCheck))
+		return MIG_TYPE_ERROR;
+#endif	/* TypeCheck */
+
+	*fb = OutP->fb;
+
+	return KERN_SUCCESS;
+}
+
+/* Routine kgi_setup_mmap_fb */
+mig_external kern_return_t kgi_setup_mmap_fb
+(
+	io_t io_object
+)
+{
+	typedef struct {
+		mach_msg_header_t Head;
+	} Request;
+
+	typedef struct {
+		mach_msg_header_t Head;
+		mach_msg_type_t RetCodeType;
+		kern_return_t RetCode;
+	} Reply;
+
+	union {
+		Request In;
+		Reply Out;
+	} Mess;
+
+	register Request *InP = &Mess.In;
+	register Reply *OutP = &Mess.Out;
+
+	mach_msg_return_t msg_result;
+
+	auto const mach_msg_type_t RetCodeCheck = {
+		/* msgt_name = */		MACH_MSG_TYPE_INTEGER_32,
+		/* msgt_size = */		32,
+		/* msgt_number = */		1,
+		/* msgt_inline = */		TRUE,
+		/* msgt_longform = */		FALSE,
+		/* msgt_deallocate = */		FALSE,
+		/* msgt_unused = */		0
+	};
+
+	InP->Head.msgh_bits =
+		MACH_MSGH_BITS(19, MACH_MSG_TYPE_MAKE_SEND_ONCE);
+	/* msgh_size passed as argument */
+	InP->Head.msgh_request_port = io_object;
+	InP->Head.msgh_reply_port = mig_get_reply_port();
+	InP->Head.msgh_seqno = 0;
+	InP->Head.msgh_id = 99007;
+
+	msg_result = mach_msg(&InP->Head, MACH_SEND_MSG|MACH_RCV_MSG|MACH_MSG_OPTION_NONE, 24, sizeof(Reply), InP->Head.msgh_reply_port, MACH_MSG_TIMEOUT_NONE, MACH_PORT_NULL);
+	if (msg_result != MACH_MSG_SUCCESS) {
+		mig_dealloc_reply_port(InP->Head.msgh_reply_port);
+		return msg_result;
+	}
+	mig_put_reply_port(InP->Head.msgh_reply_port);
+
+	if (OutP->Head.msgh_id != 99107) {
+		if (OutP->Head.msgh_id == MACH_NOTIFY_SEND_ONCE)
+		return MIG_SERVER_DIED;
+		else {
+			mig_dealloc_reply_port(InP->Head.msgh_reply_port);
+			return MIG_REPLY_MISMATCH;
+		}
+	}
+
+#if	TypeCheck
+	if ((OutP->Head.msgh_bits & MACH_MSGH_BITS_COMPLEX) ||
+	    (OutP->Head.msgh_size != 32))
+		return MIG_TYPE_ERROR;
+#endif	/* TypeCheck */
+
+#if	TypeCheck
+	if (BAD_TYPECHECK (&OutP->RetCodeType, &RetCodeCheck))
+		return MIG_TYPE_ERROR;
+#endif	/* TypeCheck */
+
+	return OutP->RetCode;
+}
